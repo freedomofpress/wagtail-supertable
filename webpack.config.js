b8aab36828c9d89a2b57c1215edc8a64f6d2e4cf
Paul Schreiber <paul@freedom.press>
2026-08-24 10:42:03 -0400

    build: remove dead CSS build pipeline
    mini-css-extract-plugin, css-loader, postcss-loader, sass-loader, and sass were configured in webpack.config.js but never did anything: no .scss file exists in the repo and the JS entry point never imports a stylesheet, so the .scss/.css module rules never matched and MiniCssExtractPlugin never emitted a .css asset. The project's actual stylesheets are plain hand-written CSS served directly via Django's static files, unrelated to webpack. Confirmed via build output (table_block.js only, no .css) before and after removal.

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,9 @@
 var webpack       = require('webpack');
 var { merge }     = require('webpack-merge');
-var MiniCssExtractPlugin = require('mini-css-extract-plugin');
-var path = require('path');
 
 var TARGET = process.env.npm_lifecycle_event;
 
 var target = __dirname + '/wagtailsupertable/static/js/';
-
-var STATIC_URL = process.env.STATIC_URL || '/static/';
-var sassData = '$static-url: "' + STATIC_URL + '";';
-console.log('Using STATIC_URL', STATIC_URL);
-
 
 var common = {
 	entry: {
@@ -25,39 +18,7 @@ var common = {
 	resolve: {
 		extensions: ['.js'],
 		modules: ['node_modules']
-	},
-
-	module: {
-		rules: [
-			{
-				test: /\.s[ca]ss$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					'css-loader',
-					'postcss-loader',
-					{
-						loader: 'sass-loader',
-						options: {
-							includePaths: [path.resolve(__dirname, 'node_modules/')],
-							data: sassData
-						}
-					}
-				],
-			},
-			{
-				test: /\.css$/,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
-			}
-		]
-	},
-
-	plugins: [
-		new MiniCssExtractPlugin({
-			filename: (getPath) => {
-				return getPath('[name].css');
-			}
-		}),
-	]
+	}
 };
 
 if (TARGET === 'build') {
